2b9e8c61f56917b9de6bab6adcfb5a2adf85e19e
Fitsum Kifle <fitsumt835@gmail.com>
2025-03-29 01:07:23 +0300

    tailwin added the project

--- furnithure/src/App.js
+++ furnithure/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export default function App() {
   return (
-    <div className='bg-gray-100'>
-      kal
+    <div className="bg-gray-100">
+      <header>
+        <ul>
+          <li className="text-2xl text-center font-bold">Furniture Store</li>
+          <li className="text-lg text-center">
+            Welcome to our online furniture store!
+          </li>
+          <li className="text-lg text-center">
+            Explore our collection of stylish and modern furniture.
+          </li>
+          <li className="text-lg text-center">
+            Shop now and transform your space!
+          </li>
+        </ul>
+      </header>
     </div>
-  )
+  );
 }
