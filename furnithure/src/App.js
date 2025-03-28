994bc6156c109ce0a4060c77e156257832d01b94
Fitsum Kifle <fitsumt835@gmail.com>
2025-03-29 01:37:23 +0300

    handle files

--- furnithure/src/App.js
+++ furnithure/src/App.js
@@ -3,16 +3,15 @@ import React from "react";
 export default function App() {
   return (
     <div className="bg-gray-100">
+        <h1 className="text-"></h1>
       <header>
-        <ul>
+        <ul className="flex  flex-row m-2 justify-between">
           <li className="text-2xl text-center font-bold">Furniture Store</li>
-          <li className="text-lg text-center">
-         kale sera edi
-          </li>
+          <li className="text-lg text-center">kale sera edi</li>
           <li className="text-lg text-center">
             Explore our collection of stylish and modern furniture.
           </li>
-          <li className="text-lg text-center h-10">
+          <li className="text-lg text-center h-1  h-10">
             Shop now and transform your space!
           </li>
         </ul>
