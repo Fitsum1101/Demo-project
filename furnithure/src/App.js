0bd020e26558b80072ccc95191111a9c309b6526
tewodrosgetu <tewodrosgetu18@gmail.com>
2025-03-29 12:50:17 -0700

    try some part

--- furnithure/src/App.js
+++ furnithure/src/App.js
@@ -1,19 +1,15 @@
 import React from "react";
-<<<<<<< HEAD
-import Exrecise from "./Teda/exrecise";
-=======
+
+import Exrecise from "./Teda/Exrecise";
 import Whychooseus from "./why choose us/Whychooseus";
 
->>>>>>> 38fbfb0af852bb349ea3fb3ee738f53812b49712
 export default function App() {
   return (
-    <div className="bg-gray-100">
+    <div className="bg-slate-600 px-20">
       <header>
         <ul>
           <li className="text-2xl text-center font-bold">Furniture Store</li>
-          <li className="text-lg text-center">
-         kale sera edi
-          </li>
+          <li className="text-lg text-center">kale sera edi</li>
           <li className="text-lg text-center">
             Explore our collection of stylish and modern furniture.
           </li>
@@ -22,13 +18,11 @@ export default function App() {
           </li>
         </ul>
       </header>
-<<<<<<< HEAD
+
       <div>
         <Exrecise />
       </div>
-=======
-    <Whychooseus/>
->>>>>>> 38fbfb0af852bb349ea3fb3ee738f53812b49712
+      <Whychooseus />
     </div>
   );
 }
--- furnithure/src/Teda/Exrecise.js
+++ furnithure/src/Teda/Exrecise.js
@@ -1,7 +1,93 @@
+import img1 from "./image/img1.jpg";
+import img2 from "./image/img2.jpg";
+import img3 from "./image/img3.jpg";
+
 function Exrecise() {
   return (
     <div>
-      <h2>teda</h2>
+      <div className="grid grid-cols-2">
+        <div className="flex">
+          <span>
+            <img className="w-80" src={img1} alt="img1" />
+          </span>
+          <span>
+            <img className="w-80" src={img2} alt="img1" />
+          </span>
+          <span>
+            <img className="w-80" src={img3} alt="img1" />
+          </span>
+          <span>
+            <img src="" alt="" />
+          </span>
+          <span>
+            <img src="" alt="" />
+          </span>
+        </div>
+        <div className="content-center justify-items-center space-y-3  ">
+          <div className="justify-items-center mb-8 text-white">
+            <h1 className="font-bold text-2xl"> WE HELP YOU MAKE</h1>
+            <h1 className="font-bold text-2xl">MODERN INTERIOR DESIGN</h1>
+          </div>
+          <p className="capitalize w-[18rem] hover:translate-y-2  bg-slate-500 text-white p-5">
+            Modern Interior Design Emphisize Simplicity, Clean, And a natiral
+            Color platelate. It focus On Functionality And minmalism, Often
+            Incorpratin natural materials And open space to create a fresh and
+            inviting atmposphere
+          </p>
+        </div>
+      </div>
+      <div className="mt-28 grid lg:grid-cols-4 sm:grid-cols-2 gap-10 ">
+        <div className=" content-center justify-items-center space-y-3 p-3 hover:bg-slate-400 rounded-xl">
+          <img
+            src="https://tailone.tailwindtemplate.net/src/img/dummy/avatar3.png"
+            class="max-w-full h-auto  rounded-full bg-gray-50 grayscale"
+            alt="titleimage"
+          />
+          <h1 className="font-bold capitalize text-xl">minmalism</h1>
+          <p className=" text-lg font-thin ">
+            Focus on simplicity and decluttered space ,using essential elements
+            to create a Clean look.
+          </p>
+        </div>
+        <div className=" content-center justify-items-center space-y-3 p-3 hover:bg-slate-400 rounded-xl ">
+          <img
+            src="https://tailone.tailwindtemplate.net/src/img/dummy/avatar1.png"
+            class="max-w-full h-auto  rounded-full bg-gray-50 grayscale"
+            alt="titleimage"
+          />
+          <h1 className="font-bold capitalize text-xl">
+            neutral color palette
+          </h1>
+          <p className=" text-lg font-thin ">
+            utlitizes muted tones such as white ,gray,and beiges,often accented
+            with bold color for contrast.
+          </p>
+        </div>
+        <div className=" content-center justify-items-center space-y-3 p-3 hover:bg-slate-400 rounded-xl">
+          <img
+            src="https://tailone.tailwindtemplate.net/src/img/dummy/avatar2.png"
+            class="max-w-full h-auto  rounded-full bg-gray-50 grayscale"
+            alt="titleimage"
+          />
+          <h1 className="font-bold capitalize text-xl">functional furniture</h1>
+          <p className=" text-lg font-thin ">
+            incorporates multi-purpose and streamlined furniture that enhances
+            usability without sacrificing style
+          </p>
+        </div>
+        <div className=" content-center justify-items-center space-y-3 p-3 hover:bg-slate-400 rounded-xl">
+          <img
+            src="https://tailone.tailwindtemplate.net/src/img/dummy/avatar3.png"
+            class="max-w-full h-auto  rounded-full bg-gray-50 grayscale"
+            alt="titleimage"
+          />
+          <h1 className="font-bold capitalize text-xl">natural materials</h1>
+          <p className=" text-lg font-thin">
+            emphasizes the use of wood, stone, and metal, bringing an organic
+            feel into modern spaces while promoting sustainability
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
