66502d4c77f265d36c12afad71db05374a9fe330
kalkidan12-k <kkflemariam@gmail.com>
2025-03-29 23:54:45 -0700

    create image folder

--- furnithure/src/why choose us/Whychooseus.js
+++ furnithure/src/why choose us/Whychooseus.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { FaShippingFast, FaTruck } from 'react-icons/fa'
+import { FaCheckCircle, FaDesktop, FaHandshakeAltSlash, FaMobileAlt, FaNetworkWired, FaRegHandshake, FaShippingFast } from 'react-icons/fa'
+import { FaHandshake } from 'react-icons/fa6'
+import photo from './image/photo.jpg';
 
 
 export default function Whychooseus() {
@@ -8,13 +10,76 @@ export default function Whychooseus() {
     <h1 className='text-center font-bold text-2xl  '>  WHY CHOOSE US</h1>  
     <h2 className='text-center font-semibold'> QUALITY CRAFTSMANSHIP
 </h2>
-<p className='px-10 text-center'>AT OUR DESIGN STUDIO, WE PRIDE OURSELVES ON USING ONLY THE FINEST MATERIALS AND<br/>
+<p className='px-10 text-center mb-24'>AT OUR DESIGN STUDIO, WE PRIDE OURSELVES ON USING ONLY THE FINEST MATERIALS AND<br/>
   TECHNIQUES. EACH PIECE OF FURNITURE IS CRAFTED WITH CARE, ENSURING DURABILITY AND STYLE
  <br/>THAT LASTS</p>
- <div>
+ <div className='grid grid-cols-3 py-10 px-10'> 
   <div>
-    <FaShippingFast/>
+  <div>
+    <FaShippingFast className='text-8xl filter grayscale'/>
+    <h2 className='font-semibold py-2'>FAST  & FREE SHIPPING</h2>
+    <p className=' capitalize'>
+    WE UNDERSTAND THAT
+<br/> WHEN YOU ORDER
+<br/> FURNITURE, YOU WANT IT
+ <br/>DELIVERED QUICKLY AND
+ <br/>WITHOUT HASSLE
+    </p>
   </div>
+  <div>
+    <FaHandshake className='text-8xl '/>
+    <h2 className='font-semibold py-2  '> 24/7 SUPPORT</h2>
+    <p>
+
+<br/>OUR SATISFACTION IS OUR PRIORITY,
+  <br/>WHICH IS WHY WE OFFER ROUND
+   <br/>THE-CLOCK SUPPORT! WHETHER
+ <br/>YOU HAVE QUESTIONS ABOUT A
+ <br/>PRODUCT, NEED ASSISTANCE WITH
+ <br/>YOUR ORDER, OR REQUIRE DESIGN
+ <br/>ADVICE, OUR DEDICATED TEAM IS
+ <br/>HERE TO HELP ANYTIME
+    </p>
+  </div>
+ </div>
+ <div>
+  <div><div className='flex text-6xl'>
+    <FaMobileAlt/><FaNetworkWired/><FaDesktop/>
+  </div>
+ <h2 className='font-semibold py-2'>
+ EASY TO SHOP
+ </h2>
+ <p>
+ <br/>HOPPING FOR FURNITURE SHOULD
+ <br/> BE A BREEZE, AND WE MAKE IT
+  <br/>SIMPLE! OUR USER-FRIENDLY
+  <br/>WEBSITE IS DESIGNED WITH YOU
+  <br/>IN MIND, ALLOWING YOU TO
+ <br/>BROWSE OUR COLLECTION
+ <br/>EFFORTLESSLY
+ </p>
+  </div>
+  <div>
+    <FaCheckCircle className='text-8xl' />
+    <h2>HASSLE FREE RETURNS</h2>
+
+    <p>
+    <br/>ENJOY WORRY-FREE SHOPPING
+ <br/>WITH OUR HASSLE-FREE
+ <br/>RETURNS! IF YOU’RE NOT
+ <br/>SATISFIED WITH YOUR
+ <br/>PURCHASE, SIMPLY LET US
+ <br/>KNOW—RETURNING ITEMS IS
+ <br/>EASY AND STRAIGHTFORWARD.
+ <br/>YOUR SATISFACTION IS OUR
+ <br/>PRIORITY
+
+    </p>
+  </div>
+ </div>
+ <div>
+  <img src={photo} alt='p'/>
+ </div>
  </div>
 
     </div>
