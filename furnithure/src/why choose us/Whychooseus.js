d8830dc42d2c7c4d53dff46191599c43dad072a7
kalkidan12-k <kkflemariam@gmail.com>
2025-03-29 22:44:03 -0700

    delivery car icon

--- furnithure/src/why choose us/Whychooseus.js
+++ furnithure/src/why choose us/Whychooseus.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import { FaShippingFast, FaTruck } from 'react-icons/fa'
+
 
 export default function Whychooseus() {
   return (
     <div>
-    <h1 className='text-center font-semibold  '>  WHY CHOOSE US</h1>  
-    <h2 className='tex'> QUALITY CRAFTSMANSHIP
+    <h1 className='text-center font-bold text-2xl  '>  WHY CHOOSE US</h1>  
+    <h2 className='text-center font-semibold'> QUALITY CRAFTSMANSHIP
 </h2>
-<p>AT OUR DESIGN STUDIO, WE PRIDE OURSELVES ON USING ONLY THE FINEST MATERIALS AND
- TECHNIQUES. EACH PIECE OF FURNITURE IS CRAFTED WITH CARE, ENSURING DURABILITY AND STYLE
- THAT LASTS</p>
+<p className='px-10 text-center'>AT OUR DESIGN STUDIO, WE PRIDE OURSELVES ON USING ONLY THE FINEST MATERIALS AND<br/>
+  TECHNIQUES. EACH PIECE OF FURNITURE IS CRAFTED WITH CARE, ENSURING DURABILITY AND STYLE
+ <br/>THAT LASTS</p>
+ <div>
+  <div>
+    <FaShippingFast/>
+  </div>
+ </div>
+
     </div>
   )
 }
